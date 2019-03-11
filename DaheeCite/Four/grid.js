acb6e43a954fe3aaaf194378d2b1f495170c9a15
Your NameDAHEE <daheelee@hotmail.com>
2019-03-11 15:49:28 -0400

    changes

---
+++ DaheeCite/Four/grid.js
@@ -0,0 +1,18 @@
+
+
+
+var i;
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
