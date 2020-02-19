54cd1a0f2d76bc2bddc038b960b907e05a6ee313
Your NameDAHEE <daheelee@hotmail.com>
2020-02-19 11:45:40 -0500

    FLORENCE IS FINALLY FUCKING DONE

--- about.js
+++ about.js
@@ -1,42 +1,35 @@
-var canvas;
-var mic;
-var angle;
+this.focus();// means you don't have to click on the canvas to use the keyboard
 
-var xoff = 0.0;
-var angle = 0;
+let g;
+let a;
+function setup() {
+  createCanvas(400, 400);
+  let inset = 40;
+  let columns = 5;
+  let rows = 5;
+  /* Optional: The number of columns and rows is the same because
+     the canvas width and height are the same.  But if the canvas
+     width and height are different, things break. Change the gridSize      calculation so that it builds square cells to fit the 
+     larger dimension. */
+  let gridSize = (width - 2 * inset) / columns;
+  g = new Grid(columns, rows, gridSize, inset);
 
-function windowResized() 
-{
-  //console.log('resized');
-  resizeCanvas(windowWidth, windowHeight);
+  a = new Agent(0, 0);
+  g.drawAgent(a);
 }
 
-function setup() 
-{
-	// fill(243, 237, 218);
-  canvas = createCanvas(windowWidth, windowHeight * 2);
-  canvas.position(0, 0);
-  canvas.style('z-index','-1');
-  frameRate(60);
+function draw() {
+  background(220);
+  g.addStuff(a);
+  g.draw();
+  g.drawAgent(a);
 }
 
+function keyPressed() {
+  if (keyCode === UP_ARROW) a.moveUp();
+  else if (keyCode === LEFT_ARROW) a.moveLeft();
+  else if (keyCode === RIGHT_ARROW) a.moveRight();
+  else if (keyCode === DOWN_ARROW) a.moveDown();
+} 
 
-function draw()
-{
-	
-	noStroke();
-	
-	xoff = xoff + 0.01;
-	angle += 1;
-  var n = noise(xoff) * 700;
 
-	stroke('#2E6038');
-	fill('#4C8456');
-  rect(angle,0,40,n);
-
-  stroke(255);
-  strokeWeight(.75);
-
-  rect(angle+4,0,40,n);
-
-}
